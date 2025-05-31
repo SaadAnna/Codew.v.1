917eb48899bd23372a8746bbef0a4b4fac6b4331
Saad Anna <saadannadev@gmail.com>
2025-05-31 17:06:11 +0200

    Update the content in the hero section

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -3,7 +3,7 @@ export const Hero = () => {
     <div className="py-24">
       <div className="px-4 md:px-0 lg:max-w-4xl max-w-4xl mx-auto flex flex-col items-center overflow-hidden relative [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
         <h1 className="text-5xl md:text-[70px] md:leading-none font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
-          Front-End Web Development Services for Modern, Responsive Websites
+          Front-End Web Development Services for Modern, Responsive Websites.
         </h1>
         <p className="text-lg md:text-xl text-white/70 mt-5 text-center lg:max-w-2xl max-w-4xl mx-auto">
           We build fast, responsive, and SEO-friendly websites using HTML, CSS,
