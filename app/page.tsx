bb23ac589650c449696704e3ab69cd2d99639b87
Saad Anna <saadannadev@gmail.com>
2025-06-07 16:08:45 +0200

    Finish the Services Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./sections/Navbar";
 import { Hero } from "./sections/Hero";
 import { Technology } from "./sections/Technology";
+import { Services } from "./sections/Services";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Navbar />
     <Hero />
     <Technology />
+    <Services />
     </div>
   );
 }
