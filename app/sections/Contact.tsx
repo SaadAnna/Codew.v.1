d0eafce1225a9d336af671633483e837391a49e5
Saad Anna <saadannadev@gmail.com>
2025-06-12 12:25:05 +0200

    Add some new style in the contact page

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -2,7 +2,7 @@ export const Contact = () => {
     return (
         <section className="py-24" id="contact">
        <div className="px-4  lg:max-w-4xl max-w-4xl mx-auto flex flex-col items-center overflow-hidden relative [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-       <h1 className="text-5xl md:text-[70px] md:leading-none font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+       <h1 className="text-4xl md:text-[45px] mx-auto md:leading-none max-w-2xl font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
 Contact Us       
  </h1>
        </div>
