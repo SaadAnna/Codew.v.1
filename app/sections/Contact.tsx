501c764e7e6b65755f1d7b318661542a7c4cc2bb
Saad Anna <saadannadev@gmail.com>
2025-06-13 16:02:55 +0200

    Add some edit in the contact page

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -1,14 +1,36 @@
 export const Contact = () => {
-    return (
-        <section className="py-24" id="contact">
-       <div className="px-4  lg:max-w-4xl max-w-4xl mx-auto flex flex-col items-center overflow-hidden relative [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-       <h1 className="text-4xl md:text-[45px] mx-auto md:leading-none max-w-2xl font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
-Contact Us       
- </h1>
-       </div>
-        </section>
-        
-    )
+  return (
+    <section className="py-24" id="contact">
+      <div className="px-4  lg:max-w-4xl max-w-4xl mx-auto flex flex-col items-center overflow-hidden relative">
+        <h1 className="text-4xl md:text-[45px] mx-auto md:leading-none max-w-2xl font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+          Let&apos;s work together{" "}
+        </h1>
+
+        <div className="mt-6 gap-3 mx-auto flex flex-col p-5 border border-white/20 rounded-xl ">
+          <div className="flex gap-5 flex-wrap items-center">
+            <input
+              type="text"
+              required
+              className="w-auto h-14 border border-white/4 rounded-xl placeholder:text-white/15 placeholder:text-sm"
+              placeholder="Fist name..."
+            />
+            <input
+              type="text"
+              required
+              className="w-auto h-14 border border-white/4 rounded-xl placeholder:text-white/15 placeholder:text-sm"
+              placeholder="Last name..."
+            />
+          </div>
+          <input
+            type="email"
+            required
+            className="w-auto h-14 border border-white/4 rounded-xl placeholder:text-white/15 placeholder:text-sm"
+            placeholder="Your Email..."
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
