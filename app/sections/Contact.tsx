7f0440c4c71050f1af63fd927298fb527b4893b3
Saad Anna <saadannadev@gmail.com>
2025-06-18 15:25:43 +0200

    Add some new content in the contact section

--- app/sections/Contact.tsx
+++ app/sections/Contact.tsx
@@ -3,7 +3,7 @@ export const Contact = () => {
     <section className="py-24" id="contact">
       <div className="px-4  lg:max-w-4xl max-w-4xl mx-auto flex flex-col items-center overflow-hidden relative">
         <h1 className="text-4xl md:text-[45px] mx-auto md:leading-none max-w-2xl font-semibold tracking-tight bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
-          Let&apos;s work together{" "}
+          Let&apos;s work together Now{" "}
         </h1>
 
         <div className="mt-6 gap-3 mx-auto flex flex-col p-5 border border-white/20 rounded-xl ">
